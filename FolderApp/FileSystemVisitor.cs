57bd23eea610f553225872beb0218547b69405ea
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-28 03:55:19 +0500

    Add project files.

---
+++ FolderApp/FileSystemVisitor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FolderApp
+{
+    public class FileSystemVisitor : IEnumerable<string>
+    {
+        private string PathFile { get; set; }
+        private string SearchPattern { get; set; }
+
+        delegate string PathDel(string pa);
+        public  delegate void ActionHandler(string fi);
+        public event ActionHandler Notify;
+        public FileSystemVisitor(string path, string searchPattern)
+        {
+            PathDel stringDel = y =>
+            {
+                if (path == string.Empty) 
+                {
+                    Console.WriteLine("The folder is empty. Please rewrite path:");
+                    path = Console.ReadLine();
+                    Notify?.Invoke($"Try to implement {path} to payh");
+                    return path;
+                   
+                    
+                }
+                else { return path; }
+            };
+            
+            stringDel(path);
+
+            PathFile = path;
+            SearchPattern = searchPattern;
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            string[] folders = Directory.GetDirectories(PathFile, "*", SearchOption.TopDirectoryOnly);
+            Notify?.Invoke("Start scanning");
+            for (int i = 0; i < folders.Length; i++)
+            {
+              //  Console.WriteLine(folders[i]);
+                yield return folders[i];
+                string[] files = Directory.GetFiles(folders[i], "*."+SearchPattern, SearchOption.AllDirectories);
+                foreach (var item in files)
+                {
+                    // Console.WriteLine("     " + Path.GetFileName(item));
+                    yield return "       " + Path.GetFileName(item);
+                }
+                
+                
+            }
+            Notify?.Invoke("Finish scanning");
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
---
+++ FolderApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace FolderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Show path: ");
+           
+            var path = Console.ReadLine();
+            Console.WriteLine("Please, write file extension to filter: ");
+            var fileExt = Console.ReadLine();
+            FileSystemVisitor fileSystemVisitor = new FileSystemVisitor(path, fileExt);
+            fileSystemVisitor.Notify +=DisplayMessage;
+            foreach (var item in fileSystemVisitor)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        /*public static void ListFileDirectory(string path)
+        {
+            string[] folders = Directory.GetDirectories(path);
+            for (int i=0; i<folders.Length;i++)
+            {
+                Console.WriteLine(folders[i]);
+                string[] files = Directory.GetFiles(folders[i]);
+                for (int j = 0; j < files.Length; j++)
+                {
+                    Console.WriteLine("     "+files[j]);
+                }
+            }
+        }*/
+
+        public static void DisplayMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
